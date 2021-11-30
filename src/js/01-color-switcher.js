8d7fb9fcc7f529aff6016dff7a88faa4463656c7
PiekharievaK <pechareva@gmail.com>
2021-11-30 23:23:53 +0200

    Добавляет решение задач

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+    btnStart: document.querySelector(`[data-start]`),
+    btnStop: document.querySelector(`[data-stop]`)
+};
 
+let switcherInterwalId = null;
+
+refs.btnStart.addEventListener(`click`, startColorSwitcher)
+refs.btnStop.addEventListener(`click`,  stopColorSwitcher)
+
+function startColorSwitcher() {
+    switcherInterwalId = setInterval(intervalColorSwitcher, 1000);
+};
+
+function intervalColorSwitcher() {
+    document.body.style.backgroundColor = getRandomHexColor();
+    refs.btnStart.disabled = true;}
+ 
+function stopColorSwitcher() {
+    if (!refs.btnStart.disabled) {
+        return
+    } 
+    clearInterval(switcherInterwalId);
+    refs.btnStart.disabled = false;
+ };
+
+ function getRandomHexColor() {
+   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,94 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import { Report } from 'notiflix/build/notiflix-report-aio';
+
+const timer = document.querySelector(`.timer`);
+const timerDays = timer.querySelector(`[data-days]`);
+const timerHours = timer.querySelector(`[data-hours]`);
+const timerMinutes = timer.querySelector(`[data-minutes]`);
+const timerSeconds = timer.querySelector(`[data-seconds]`);
+
+const textInput = document.querySelector(`#datetime-picker`);
+const button = document.querySelector(`button`);
+
+let diference = ''
+let convertDiference = '';
+let choiseDate = '';
+let timerInterval = null;
+let timerisActive = false;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+    minuteIncrement: 1,
+  
+    onClose(selectedDates) {
+        choiseDate = selectedDates[0];
+        button.disabled = false;
+        if (choiseDate.getTime() <= Date.now()) {
+            Report.failure(`Sorry, you pick unavailable date`, `Please choose a date in the future`, 'Okay');
+            // Notify.failure('Qui timide rogat docet negare');
+            button.disabled = true;
+            return
+        }
+      
+    },
+};
+const calendarCreate = flatpickr(textInput, options);
+ 
+button.addEventListener(`click`, diferenceTimerStart)
+
+function onStartBtnClick() {
+   
+    diference = choiseDate.getTime() - Date.now();
+    convertDiference = convertMs(diference)
+      timerDays.textContent = convertDiference.days ;
+      timerHours.textContent = convertDiference.hours ;
+      timerMinutes.textContent = convertDiference.minutes ;
+      timerSeconds.textContent = convertDiference.seconds;
+}
+
+function diferenceTimerStart() {
+  
+    if (!choiseDate || timerisActive) {
+        clearInterval(timerInterval);
+        button.textContent = `Start`;
+          textInput.disabled = false;
+           timerisActive = false;
+        return
+    }
+    timerInterval = setInterval(onStartBtnClick, 1000);
+    textInput.disabled = true;
+    timerisActive = true;
+    button.textContent = `Stop`;
+      return timerInterval;
+};
+    
+function addLeadingZero(value) {
+   return  String(value).padStart(2, 0)  
+}
+
+    function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
+
+// function choiseDate() {
+    //     return new Date(textInput._flatpickr.selectedDates[0])
+    // };
+    
